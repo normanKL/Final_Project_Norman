f5bc6eb4932fbda817e7653dab7ed21cd872aa24
Norman Cheahe <cheahhonyuen@gmail.com>
2024-01-27 12:49:37 +0800

    Completed Coding

--- src/components/pet.jsx
+++ src/components/pet.jsx
@@ -3,6 +3,10 @@ import Breed from './breed'
 
 const pet = (props) => {
 
+    const [show, setShow] = useState(false)
+
+        const handleClick = () => {setShow (!show)}
+
     return (
     <div className='pet-profile'>
         <p className='pet-breed'>
@@ -10,10 +14,22 @@ const pet = (props) => {
                 <Breed breed={breed} key={index}/>
             ))}
         </p>
-        <img src={props.pet.image} alt="image"/>
+        <img src={props.pet.image} onClick ={handleClick} alt="image"/>
         <h4 className='pet-intro'>My name is </h4>
         <h3 className='pet-name'>{props.pet.pet_name}</h3>
-        <p className='pet-know'>Click my photo to know me!</p>
+        
+        {show && 
+        <div className='pet-know'>
+            <p className='details-title'>Know More About Me!</p>
+            <p className='details'>I am a {props.pet.type}</p>
+            <p className='details'>Age: {props.pet.age}</p>
+            <p className='details'>Sex: {props.pet.sex}</p>
+            <p className='details'>Spayed/Neutered: {props.pet.spayed}</p>
+            <p className='details'>Vaccinated: {props.pet.vaccinated}</p>
+            <p className='details'>Dewormed: {props.pet.dewormed}</p>
+            <p className='details'>Posted: {props.pet.posted}</p>
+        </div>
+        }
  
     </div>
     )
